7b2085ea0fc23517a39d2de9f1b6ce1bf89b6602
nick9lee <nicholas9lee@gmail.com>
2021-10-31 17:26:55 -0600

    updated read me

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # plus15playground
 trying out tech before implementation
+
+trying to create poly lines/polygons in map
+
+
+
+
---
+++ plus 15 playground/plus 15 playground/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  plus 15 playground
+//
+//  Created by Nicholas Lee on 2021-09-12.
+//
+
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
